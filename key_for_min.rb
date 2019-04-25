ee17c432ea197cca30e366de6ceff2402f29ebd3
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-25 17:11:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     values << value
   end
-  values.sort! {|a,b| a<=>b}
-  min_value = values[0]
+  values.sort!
   name_hash.collect do |name,value|
-    if value == min_value
+    if value == values[0]
       return name
     end
   end
